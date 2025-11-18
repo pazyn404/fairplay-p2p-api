80cc75dd32a47d3047454f8a27487cde8ff56f1c
unknown <pazyn404@gmail.com>
2025-11-18 08:53:55 +0200

    System fix: violated contraint error status code - set default status code to 409

--- system/exception_handlers/violated_constraint_errors_list_handler.py
+++ system/exception_handlers/violated_constraint_errors_list_handler.py
@@ -1,4 +1,4 @@
-from fastapi import Request, HTTPException
+from fastapi import Request, HTTPException, status
 
 from app import app
 from exceptions import ViolatedConstraintErrorsList
@@ -6,4 +6,4 @@ from exceptions import ViolatedConstraintErrorsList
 
 @app.exception_handler(ViolatedConstraintErrorsList)
 def violated_constraint_errors_list_handler(request: Request, exc: ViolatedConstraintErrorsList):
-    raise HTTPException(status_code=exc.status_code, detail=exc.errors)
+    raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=exc.errors)
